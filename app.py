065313ed1c5610c5de63f5880e42a3ca9d1359b3
Efraimnegreiros <141242508+Efraimnegreiros@users.noreply.github.com>
2024-10-10 20:26:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,13 @@ def index():
     if request.method == 'POST':
         start_date = request.form['start_date']
         end_date = request.form['end_date']
+        yposition = int(request.form['yposition'])
+        tamanhofonte = int(request.form['tamanhofonte'])
+        espacamento = int(request.form['espacamento'])
         aniversariantes = buscar_aniversariantes(start_date, end_date)
         aniversariantes_casamentos = buscar_aniversariantes_casamento(start_date, end_date)
         if aniversariantes:
-            image_path = gerar_imagem(aniversariantes, aniversariantes_casamentos)
+            image_path = gerar_imagem(aniversariantes, aniversariantes_casamentos, yposition, tamanhofonte, espacamento)
             return send_file(image_path, mimetype='image/png')
 
     return render_template('index.html', aniversariantes=[])
@@ -32,33 +35,41 @@ def index():
 def buscar_aniversariantes(start_date, end_date):
     start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
     end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-    response = supabase.table('membros').select('*').gte('data_aniversario', start_date).lte('data_aniversario', end_date).execute()
+    # response = supabase.table('membros').select('*').gte('data_aniversario', start_date).lte('data_aniversario', end_date).execute()
+    response = supabase.table('membros')\
+    .select('*')\
+    .gte('data_aniversario', start_date)\
+    .lte('data_aniversario', end_date)\
+    .order('data_aniversario')\
+    .execute()
+
+
     return response.data
 def buscar_aniversariantes_casamento(start_date, end_date):
     start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
     end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
     response = supabase.table('aniversarios_casamento').select('*').gte('data_aniversario', start_date).lte('data_aniversario', end_date).execute()
     return response.data
-def gerar_imagem(aniversariantes, aniversariantes_casamentos):
+def gerar_imagem(aniversariantes, aniversariantes_casamentos, y_position=275, tamanhofonte=40, espacamento=55):
     # Carregar a imagem padrão
     image = Image.open('static/padrao.png')
     draw = ImageDraw.Draw(image)
     font = ImageFont.load_default()  # Você pode usar uma fonte específica se preferir
 
     # Adicionar texto dos aniversariantes
-    y_position = 275  # Posição inicial do texto
+    # y_position = 275  # Posição inicial do texto
     for membro in aniversariantes:
-        font_geral = ImageFont.truetype('GlacialIndifference-Bold.otf', 40)
+        font_geral = ImageFont.truetype('GlacialIndifference-Bold.otf', tamanhofonte)
         nome = membro['nome']
         data_aniversario = membro['data_aniversario']
         data_aniversario = datetime.strptime(data_aniversario, "%Y-%m-%d")
         data_aniversario = data_aniversario.strftime('%d/%m')
         draw.text((157, y_position), f"{nome}", fill='black', font=font_geral)
         draw.text((800, y_position), f"{data_aniversario}", fill='black', font=font_geral)
-        y_position += 55  # Espaço entre as linhas
+        y_position += espacamento  # Espaço entre as linhas
     y_position = 900
     for membro in aniversariantes_casamentos:
-        font_geral = ImageFont.truetype('GlacialIndifference-Bold.otf', 40)
+        font_geral = ImageFont.truetype('GlacialIndifference-Bold.otf', tamanhofonte)
         nome = membro['nome']
         data_aniversario = membro['data_aniversario']
         data_aniversario = datetime.strptime(data_aniversario, "%Y-%m-%d")
